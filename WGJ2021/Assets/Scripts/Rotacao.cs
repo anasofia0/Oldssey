2a06e834171c4debaaad5b60c5670740fe940587
wirlianny <wirlianny@gmail.com>
2021-08-21 15:42:18 -0300

    alteracoes

---
+++ WGJ2021/Assets/Scripts/Rotacao.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacao : UnityEngine.MonoBehaviour
+{
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        Screen.autorotateToLandscapeLeft = true;
+        Screen.autorotateToLandscapeRight = true;
+        Screen.autorotateToPortrait = false;
+        Screen.autorotateToPortraitUpsideDown = false;
+        Screen.orientation = ScreenOrientation.AutoRotation;
+    }
+}
